38336e664ccefd0412f8bb32f32df0d592a8b9a9
Joey Santoro <joseph.santoro@duke.edu>
2022-03-19 16:56:41 -0700

    dynamicRewards

---
+++ src/rewards/FuseFlywheelRewardsPlugin.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.10;
+
+import {SafeTransferLib, ERC20} from "solmate/utils/SafeTransferLib.sol";
+
+import {IFlywheelRewards} from "flywheel/interfaces/IFlywheelRewards.sol";
+import {FlywheelCore} from "flywheel/FlywheelCore.sol";
+
+interface ICERC20 {
+    function plugin() external returns (address);
+}
+
+interface IERC4626 {
+    function claimRewards() external;
+}
+
+/** 
+ @title Fuse Flywheel Plugin Reward Stream
+ @notice Determines rewards based on how many reward tokens appeared in the market itself since last accrual.
+ All rewards are claimed atomically, so there is no need to use the last reward timestamp.
+*/
+contract FuseFlywheelDynamicRewards is IFlywheelRewards {
+    using SafeTransferLib for ERC20;
+
+    event Log(string message);
+
+    /// @notice the reward token paid
+    ERC20 public immutable override rewardToken;
+
+    /// @notice the flywheel core contract
+    FlywheelCore public immutable flywheel;
+
+    constructor(ERC20 _rewardToken, FlywheelCore _flywheel) {
+        rewardToken = _rewardToken;
+        flywheel = _flywheel;
+    }
+
+    /**
+     @notice calculate and transfer accrued rewards to flywheel core
+     @param market the market to accrue rewards for
+     @return amount the amount of tokens accrued and transferred
+     */
+    function getAccruedRewards(ERC20 market, uint32)
+        external
+        override
+        returns (uint256 amount)
+    {
+        require(msg.sender == address(flywheel), "!flywheel");
+        IERC4626 plugin = IERC4626(ICERC20(address(market)).plugin());
+        try plugin.claimRewards() {} catch {
+            emit Log("!plugin.claimRewards()");
+        }
+        amount = rewardToken.balanceOf(address(market));
+        if (amount > 0)
+            rewardToken.safeTransferFrom(
+                address(market),
+                address(flywheel),
+                amount
+            );
+    }
+}

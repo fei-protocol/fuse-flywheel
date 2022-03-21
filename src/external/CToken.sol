b15ae17230c5d586e7930477555da09ec70445fc
Joey Santoro <joseph.santoro@duke.edu>
2022-03-20 18:49:50 -0700

    add plugin vaults

---
+++ src/external/CToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import {CERC20} from "libcompound/interfaces/CERC20.sol";
+
+abstract contract CToken is CERC20 {
+    function comptroller() external view virtual returns (address);
+
+    function getCash() external view virtual returns (uint256);
+}

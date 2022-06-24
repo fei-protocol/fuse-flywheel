59071fc1739b2b1747bde6ce0a89eb4790858e5b
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-06-24 13:41:55 +0300

    using the forge-std/Test.sol implementation of bound()

--- src/test/utils/BaseDSTest.sol
+++ src/test/utils/BaseDSTest.sol
@@ -19,18 +19,6 @@ contract BaseDSTest is DSTestPlus, Test {
         uint256 min,
         uint256 max
     ) public override(DSTestPlus, Test) returns (uint256 result) {
-        require(max >= min, "MAX_LESS_THAN_MIN");
-
-        uint256 size = max - min;
-
-        if (size == 0) result = min;
-        else if (size == type(uint256).max) result = x;
-        else {
-            ++size; // Make max inclusive.
-            uint256 mod = x % size;
-            result = min + mod;
-        }
-
-        emit log_named_uint("Bound Result", result);
+        return Test.bound(x, min, max);
     }
 }
